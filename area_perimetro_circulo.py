cc78d78847278c026a14139d11957ac099afeac1
Michaell Sebastian Morales García <michaelmorales@colegioguanenta.edu.co>
2026-01-30 07:37:35 -0500

    Add files via upload

--- area_perimetro_circulo.py
+++ area_perimetro_circulo.py
@@ -1,1 +1,24 @@
+#Programa para calcular el área y el perimetro de un circulo de radio R
 
+#Librerias
+import math
+
+print("----------------------------------")
+print("---Area y perimetro del circulo---")
+print("----------------------------------")
+
+#input
+r = input("Ingrese el valor del radio del circulo: ")
+r = int(r)
+
+#procesing
+a = math.pi*r**2
+p= 2*math.pi*r
+
+#output
+print("----------------------------------")
+print("-----------Resultados-------------")
+print("----------------------------------")
+print("El área del círculo es: " +str(a))
+print("El perimetro del circulo es: " + str(p))
+print("------------------------------------")
