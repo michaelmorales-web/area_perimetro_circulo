55ab1b3acea9a72a5a1598500872214c26c9679d
Michaell Sebastian Morales García <michaelmorales@colegioguanenta.edu.co>
2026-01-30 06:16:50 -0500

    Update area_perimetro_circulo.py

--- area_perimetro_circulo.py
+++ area_perimetro_circulo.py
@@ -1,6 +1,7 @@
 #programa para calcular el área de un circulo
 
 #inicio
+import math
 r = input("digite el radio del circulo: ")
 π = 3,1416
 a = 0
@@ -15,3 +16,11 @@ r = int(r)
 
 a = π * (r * r)
 p = 2 * π * r
+
+#output :P
+print("el área del circulo es: " + str(a))
+print("el preimetro del circulo es:" + str(p))
+
+print()
+print("(presione Enter para salir)")
+input()
